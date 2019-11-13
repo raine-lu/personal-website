54988687c8ad420c68ceb9090c4fd7d8020ec41c
raine-lu <57714698+raine-lu@users.noreply.github.com>
2019-11-14 02:35:14 +0800

    Update homepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./components/Home";
 import { createMuiTheme, CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
-import Rainy from "./rainy.jpg";
+import Particles from "react-particles-js";
 
 const theme = createMuiTheme({
   overrides: {
@@ -16,34 +16,14 @@ const theme = createMuiTheme({
 
 function App() {
   return (
-    <div
-      style={{
-        backgroundColor: "#000",
-        margin: 0,
-        minHeight: "100vh",
-        padding: "0px",
-        border: "0px",
-        margin: "0px",
-        minWidth: "100vw" 
-      }}
-    >
-      <div
-        style={{
-          backgroundSize: "cover",
-          backgroundAttachment: "fixed",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundImage: "url(" + Rainy + ")", //syntax for background image
-          opacity: 0.75
-        }}
-      > 
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Home />
-        </ThemeProvider>
-      </div>
-     </div> 
+    <div style={{ backgroundColor: "#000" }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Home />
+      </ThemeProvider>
+    </div>
   );
 }
+
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,65 +10,95 @@ import {
 import FaceIcon from "@material-ui/icons/Face";
 import ResumeIcon from "@material-ui/icons/Description";
 import ContactIcon from "@material-ui/icons/ContactMail";
+import Particles from "react-particles-js";
 
 function Home() {
   return (
-    <Grid
-      container
-      spacing={1}
-      alignItems="center"
-      justify="center"
-      direction="column"
-      style={{ minHeight: "100vh" }}
-    >
-      <Box style={{ backgroundColor: "#000" }}>
-        <Typography variant="h2">
-          Hi I'm <b>Raine</b>
-        </Typography>
-
-        <Typography
-          variant="subtitle2"
-          display="block"
-          style={{ marginTop: "1em" }}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          <br /> Quos blanditiis tenetur unde suscipit, <br />
-          quam beatae rerum inventore consectetur,
-          <br /> neque doloribus, cupiditate numquam dignissimos laborum
-          <br />
-          fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
-      </Box>
-      <GridList
-        cellHeight={"10em"}
-        col={3}
-        spacing={2}
+    <div>
+      <Particles
+        params={particleParams}
         style={{
-          width: "27em",
-          marginTop: "8em"
+          position: "absolute",
+          height: "100%",
+          width: "100%"
         }}
+      />
+      <Grid
+        container
+        spacing={1}
+        alignItems="center"
+        justify="center"
+        direction="column"
+        style={{ minHeight: "100vh" }}
       >
-        <GridListTile style={{ width: "33.3%" }}>
-          <ButtonBase>
-            <FaceIcon style={{ color: "#fff", marginRight: "8px" }} />
-            <Typography variant="button">About</Typography>
-          </ButtonBase>
-        </GridListTile>
-        <GridListTile style={{ width: "33.3%" }}>
-          <ButtonBase>
-            <ResumeIcon style={{ color: "#fff", marginRight: "8px" }} />
-            <Typography variant="button">Resume</Typography>
-          </ButtonBase>
-        </GridListTile>
-        <GridListTile style={{ width: "33.3%" }}>
-          <ButtonBase>
-            <ContactIcon style={{ color: "#fff", marginRight: "8px" }} />
-            <Typography variant="button">Contact</Typography>
-          </ButtonBase>
-        </GridListTile>
-      </GridList>
-    </Grid>
+        <Box style={{ backgroundColor: "#000" }}>
+          <Typography variant="h2">
+            Hi I'm <b>Raine</b>
+          </Typography>
+
+          <Typography
+            variant="subtitle2"
+            display="block"
+            style={{ marginTop: "1em" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            <br /> Quos blanditiis tenetur unde suscipit, <br />
+            quam beatae rerum inventore consectetur,
+            <br /> neque doloribus, cupiditate numquam dignissimos laborum
+            <br />
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </Typography>
+        </Box>
+        <GridList
+          cellHeight={"10em"}
+          col={3}
+          spacing={2}
+          style={{
+            width: "27em",
+            marginTop: "8em"
+          }}
+        >
+          <GridListTile style={{ width: "33.3%" }}>
+            <ButtonBase>
+              <FaceIcon style={{ color: "#fff", marginRight: "8px" }} />
+              <Typography variant="button">About</Typography>
+            </ButtonBase>
+          </GridListTile>
+          <GridListTile style={{ width: "33.3%" }}>
+            <ButtonBase>
+              <ResumeIcon style={{ color: "#fff", marginRight: "8px" }} />
+              <Typography variant="button">Resume</Typography>
+            </ButtonBase>
+          </GridListTile>
+          <GridListTile style={{ width: "33.3%" }}>
+            <ButtonBase>
+              <ContactIcon style={{ color: "#fff", marginRight: "8px" }} />
+              <Typography variant="button">Contact</Typography>
+            </ButtonBase>
+          </GridListTile>
+        </GridList>
+      </Grid>
+    </div>
   );
 }
+
+const particleParams = {
+  particles: {
+    number: {
+      value: 50
+    },
+    size: {
+      value: 3
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+};
 
 export default Home;
