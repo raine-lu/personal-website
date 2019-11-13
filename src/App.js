946d72137d28ce098144dcbed63d024bdfa5d2f6
raine-lu <57714698+raine-lu@users.noreply.github.com>
2019-11-13 22:45:19 +0800

    main background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import Home from './components/Home'
+import React from "react";
+import Home from "./components/Home";
+import { createMuiTheme, CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import Rainy from "./rainy.jpg";
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiContainer: {
+      root: {
+        width: "50%",
+        left: "50%",
+        top: "50%",
+        overflow: "auto"
+      }
+    }
+  }
+});
 
 function App() {
   return (
-    <div>
-      <Home/>
+    <div
+      style={{
+        minHeight: "100vh",
+        padding: "0px",
+        border: "0px",
+        margin: "0px",
+        minWidth: "100vw",
+        backgroundColor: "#000",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundImage: "url(" + Rainy + ")", //syntax for background image
+        opacity: 0.75
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Home />
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Grid, Typography } from "@material-ui/core";
 
 function Home() {
-  return <div></div>;
+  return (
+    <Grid container spacing={1} alignItems="center" justify="center" direction="column" style={{minHeight:'100vh'}}>
+        <Typography variant="h2" style={{color: "#fff"}}>
+            Hi I'm Raine
+        </Typography>
+    </Grid>
+  );
 }
 
 export default Home;
