2ce1a7fb619f59e77078196a2b3a3cba92cf43c5
raine-lu <57714698+raine-lu@users.noreply.github.com>
2019-11-14 04:31:00 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import {
   Typography,
   GridList,
   GridListTile,
-  ButtonBase,
+  ButtonBase
 } from "@material-ui/core";
 import { Link, withRouter } from "react-router-dom";
 import FaceIcon from "@material-ui/icons/Face";
@@ -13,59 +13,59 @@ import ContactIcon from "@material-ui/icons/ContactMail";
 
 function Home() {
   return (
-      <Grid
-        container
-        spacing={1}
-        alignItems="center"
-        justify="center"
-        direction="column"
-        style={{ minHeight: "100vh", padding: "10px" }}
-      >
-        <Typography variant="h3">
-          Hi I'm <b>Raine</b>
-        </Typography>
+    <Grid
+      container
+      spacing={1}
+      alignItems="center"
+      justify="center"
+      direction="column"
+      style={{ minHeight: "100vh", padding: "10px" }}
+    >
+      <Typography variant="h3">
+        Hi I'm <b>Raine</b>
+      </Typography>
 
-        <Typography
-          variant="subtitle2"
-          display="block"
-          style={{ marginTop: "1em" }}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          <br /> Quos blanditiis tenetur unde suscipit, <br />
-          quam beatae rerum inventore consectetur,
-          <br /> neque doloribus, cupiditate numquam dignissimos laborum
-          <br />
-          fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
-        <GridList
-          cellHeight={"10em"}
-          col={3}
-          spacing={2}
-          style={{
-            width: "27em",
-            marginTop: "8em"
-          }}
-        >
-          <GridListTile style={{ width: "33.3%" }}>
-            <ButtonBase component={Link} to={"/about"}>
-              <FaceIcon style={{ color: "#fff", marginRight: "8px" }} />
-              <Typography variant="button">About</Typography>
-            </ButtonBase>
-          </GridListTile>
-          <GridListTile style={{ width: "33.3%" }}>
-            <ButtonBase>
-              <ResumeIcon style={{ color: "#fff", marginRight: "8px" }} />
-              <Typography variant="button">Resume</Typography>
-            </ButtonBase>
-          </GridListTile>
-          <GridListTile style={{ width: "33.3%" }}>
-            <ButtonBase>
-              <ContactIcon style={{ color: "#fff", marginRight: "8px" }} />
-              <Typography variant="button">Contact</Typography>
-            </ButtonBase>
-          </GridListTile>
-        </GridList>
-      </Grid>
+      <Typography
+        variant="subtitle2"
+        display="block"
+        style={{ marginTop: "1em" }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <br /> Quos blanditiis tenetur unde suscipit, <br />
+        quam beatae rerum inventore consectetur,
+        <br /> neque doloribus, cupiditate numquam dignissimos laborum
+        <br />
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+      <GridList
+        cellHeight={"10em"}
+        col={3}
+        spacing={2}
+        style={{
+          width: "27em",
+          marginTop: "8em"
+        }}
+      >
+        <GridListTile style={{ width: "33.3%" }}>
+          <ButtonBase component={Link} to={"/about"}>
+            <FaceIcon style={{ color: "#fff", marginRight: "8px" }} />
+            <Typography variant="button">About</Typography>
+          </ButtonBase>
+        </GridListTile>
+        <GridListTile style={{ width: "33.3%" }}>
+          <ButtonBase>
+            <ResumeIcon style={{ color: "#fff", marginRight: "8px" }} />
+            <Typography variant="button">Resume</Typography>
+          </ButtonBase>
+        </GridListTile>
+        <GridListTile style={{ width: "33.3%" }}>
+          <ButtonBase>
+            <ContactIcon style={{ color: "#fff", marginRight: "8px" }} />
+            <Typography variant="button">Contact</Typography>
+          </ButtonBase>
+        </GridListTile>
+      </GridList>
+    </Grid>
   );
 }
 
